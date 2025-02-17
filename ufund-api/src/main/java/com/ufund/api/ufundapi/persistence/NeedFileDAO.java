03400bca10fbbe26f3014e5b6a1d0e9915472bb9
= <och4609@g.rit.edu>
2025-02-17 17:04:26 -0500

    Work on updating and getting a need

--- ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/NeedFileDAO.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/NeedFileDAO.java
@@ -180,7 +180,7 @@ public class NeedFileDAO implements NeedDAO {
         synchronized(needs) {
             // We create a new Need object because the id field is immutable
             // and we need to assign the next unique id
-            Need newNeed = new Need(nextId(),need.getName(), need.getCost());
+            Need newNeed = new Need(nextId(),need.getName(), need.getCost(), need.getFoodGroup());
             needs.put(newNeed.getId(),newNeed);
             save(); // may throw an IOException
             return newNeed;
