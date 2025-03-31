b9cf4bff3a0e8bb7d184f7597f676beb776bc208
= <och4609@g.rit.edu>
2025-03-31 11:31:34 -0400

    Adding multiple css fixes

--- ufund-ui/src/app/app.component.ts
+++ ufund-ui/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import {BehaviorSubject} from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -6,6 +9,18 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+    user = new BehaviorSubject<boolean>(false);
+    constructor(
+      private router: Router,
+      private route: ActivatedRoute,
+    ){}
+
+    
   title = 'ufund-ui';
 
+  logOut(): void{
+    localStorage.removeItem('user')
+    localStorage.removeItem('role')
+    this.router.navigate([`/`])
+  }
 }
